06b4c0e0bbca5f39ae34fdb594f8c2ae5b8a37cd
kim201621123 <66352581+kim201621123@users.noreply.github.com>
2024-05-17 01:38:30 +0900

    init commit

---
+++ src/main/java/com/sparta/java_personal_task_2/JavaPersonalTask2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.java_personal_task_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPersonalTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPersonalTask2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/java_personal_task_2/JavaPersonalTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.java_personal_task_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPersonalTask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
